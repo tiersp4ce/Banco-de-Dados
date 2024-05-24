9b18cbdf788ff61ecd663ad2680aadb927cdc1a8
tiersp4ce <165806291+tiersp4ce@users.noreply.github.com>
2024-05-24 11:22:10 -0300

    Update Bliblioteca.sql

--- Bliblioteca.sql
+++ Bliblioteca.sql
@@ -1,45 +1,46 @@
 
 CREATE TABLE Livros (
-    titulo TEXT NOT NULL,
-    autor TEXT NOT NULL,
-    genero TEXT NOT NULL,
-    disponibilidade INTEGER DEFAULT 4,
-    id_livro INT PRIMARY KEY
+    id INT PRIMARY KEY AUTO_INCREMENT,
+  	titulo VARCHAR(50) NOT NULL,
+    autor VARCHAR(50) NOT NULL,
+    genero VARCHAR(50) NOT NULL,
+  	id_genero INT,
+  	FOREIGN KEY (id_genero) REFERENCES genero(id)
+);
+
+Create Table genero(
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	nome VARCHAR(50)
 );
 
 CREATE TABLE Clientes (
-    id_cliente INT NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     telefone VARCHAR(20) NOT NULL,
-    endereco VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_cliente)
+    endereco VARCHAR(255) NOT NULL
 );
 
-
 CREATE TABLE Recepcionistas (
-  id_recepcionista INT NOT NULL,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   telefone INT NOT NULL,
   nome VARCHAR(45) NOT NULL,
   email VARCHAR(45) NOT NULL,
   senha VARCHAR(45) NOT NULL,
-  PRIMARY KEY (id_recepcionista)
-  );
+);
   
 CREATE TABLE Emprestimos (
-    id_emprestimo INT NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     id_livro INT NOT NULL,
     id_cliente INT NOT NULL,
 	data_emprestimo DATE NOT NULL,
     data_devolucao DATE NOT NULL,
-    PRIMARY KEY (id_emprestimo),
     FOREIGN KEY (id_livro) REFERENCES Livros(id_livro),
     FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente)
 );
-  
-CREATE TABLE Editora(
-  id_editora INT NOT NULL,
-  nome_editora VARCHAR(45) NOT NULL,
-  PRIMARY KEY (id_editora)
-);
+
+
+insert into livros (titulo, autor, genero) values ("Bela e a fera","Andre Feire","Fantasia"),("Aprendendo em Java","HitaloYo","Educação"),("Titanic","Leonardo de Caprio","Drama"),("Harry Potter: A pedra de loló","jk Rolling","Fantasia"),("Os amigos Covardes","Wallisson Gomes","Didatico"),;
+
+insert into genero (nome) Values ("Terror"),("Educação"),("Drama"),("Fantasia"),("Didatico");
 
